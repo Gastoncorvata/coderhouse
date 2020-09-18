83f4c2a5c37b2f7d860b6c0a9156448c3247a9ad
Gaston Corvalan <corvata@gmail.com>
2020-09-17 22:10:24 -0300

    comit incial de mi repositorio

---
+++ js/action.js
@@ -0,0 +1,16 @@
+
+window.onscroll = function () {
+    scrollable();
+}
+function scrollable() {
+    var y = 0;
+    var currentY = window.pageYOffset;
+    if (y < currentY) {
+        document.querySelector('#customNavbar').classList.remove('navbar-light', 'bg-light', 'fixed-theme');
+        document.querySelector('#customNavbar').classList.add('navbar-dark', 'bg-dark', 'fixed-theme');
+    } else if (y == currentY) {
+        document.querySelector('#customNavbar').classList.remove('navbar-dark', 'bg-dark', 'fixed-theme');
+        document.querySelector('#customNavbar').classList.add('navbar-light', 'bg-light', 'fixed-theme');
+    }
+}
+scrollable();
